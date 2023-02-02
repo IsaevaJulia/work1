a7d40c720b947b8f0b494bf2fca879a46f64341d
Yulia <Lila2763@yandex.ru>
2023-02-02 21:51:10 +0300

    work1

---
+++ script.js
@@ -0,0 +1,12 @@
+// task 1
+
+let degreeCelsius = 10;
+let degreeFahrenheit = (9/5) * degreeCelsius + 32;
+console.log(`Температура по цельсию = ${degreeCelsius}°C,
+температура по Фаренгейту = ${degreeFahrenheit}°F.`)
+
+// task2
+
+const nameNew = "Yulia";
+let admin = nameNew;
+console.log(admin);
